ab2c85376f2d898fc3882f10593ae237a1ba5e01
Nixon Pastor <nixonpastorguzman@gmail.com>
2023-12-28 16:43:01 -0700

    Initial REST creation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+//Add Middleware for JSON and url encoded body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/taskManagement/tasks", require("./routes/taskRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server started on port ${port}`));
